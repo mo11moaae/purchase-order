f54c9a86eb1b2cf15a226d7e34a87ffa61d7cfa3
mo11moaae <mo11moaae@gmail.com>
2025-11-20 00:47:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GSCRIPT_URL = 'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbzjyqCVI0fbpGsrZU-RdSsXPPTLZf0PaGx3a1v8Lbptj9n6dTRk-Rx_dMpIteqxjDOrxw/exec/exec';
+const GSCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzjyqCVI0fbpGsrZU-RdSsXPPTLZf0PaGx3a1v8Lbptj9n6dTRk-Rx_dMpIteqxjDOrxw/exec';
